8a4898fdba2f66cc51ce917edc7115b7ff6b418e
Abanob Tawfik <abanob.tawfik@outlook.com>
2019-10-13 04:58:56 +1100

    submitted final files

--- Assignment1/test.sh
+++ Assignment1/test.sh
@@ -4,5 +4,11 @@ touch output.txt
 
 for i in 1 2 3 4 5 6 7
 do
-	time dafny /compile:3 Ex$i.dfy >> output.txt
+	echo "---------------------------------------------------------" >> output.txt
+	echo "running Ex$i.dfy" >> output.txt
+	echo " " >> output.txt
+	dafny /compile:3 Ex$i.dfy >> output.txt
+	echo "---------------------------------------------------------" >> output.txt
+	echo " " >> output.txt
+	echo " " >> output.txt
 done
